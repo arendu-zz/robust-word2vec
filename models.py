13d6fe2fe3ee2c198716136d9619e702cbd4e2a4
Duolingo2 <duolingo2@duolingos-MacBook-Pro.local>
2017-07-13 00:15:20 -0400

    added skipgram

--- models.py
+++ models.py
@@ -50,7 +50,7 @@ class BaseModel(object):
         self._eps = 1e-8
         self.params = []
         self.reg_params = []
-        self.pad = theano.shared(floatX(np.zeros((1, self.embed_size))), 'Pad')
+        self.set_optimizer(optimize)
         if saved_model is None:
             W_in = _get_weights('W_IN', self.vocab_model.size, self.embed_size, 'nestrov')
             W_context = _get_weights('W_CONTEXT', self.embed_size, self.vocab_model.size, 'nestrov')
@@ -58,41 +58,55 @@ class BaseModel(object):
             W_in, W_context = self.load_model(saved_model)
         self.params = [W_in, W_context]
         self.reg_params = [W_in, W_context]
-        if optimize  == 'adam':
-            self._update = adam
-        elif optimize == 'adagrad':
-            self._update = adagrad 
-        elif optimize == 'momentum':
-            self._update = momentum
-        elif optimize == 'sgd':
-            self._update = sgd
-        elif optimize == 'rms_clipped':
-            self._update = rmsprop_clipped
-        elif optimize == 'sgd_clipped':
-            self._update = sgd_clipped
-        elif optimize == 'rms':
-            self._update = rmsprop
-        else:
-            raise NotImplementedError("dont know what update this is...")
         assert isinstance(self.reg, float)
         assert isinstance(self.noise_sample_size, int) or self.noise_sample_size is None
         assert isinstance(self.noise_dist, np.ndarray) or self.noise_dist is None
         assert isinstance(self.vocab_model, Vocab)
-        #self.__make_cosine_sim_graph__()
-    """
-    def __make_cosine_sim_graph__(self,):
-        #cosine similarity methods
-        W_in = self.params[0]
-        numerator = W_in.dot(W_in.T) #(self.vocab_model.size, self.vocab_model.size)
-        l2_norms = T.sqrt(T.sum(W_in ** 2, axis = 1)) #(self.vocab_model.size)
-        demoninator = l2_norms[:, np.newaxis].dot(l2_norms[np.newaxis, :])
-        self.cosine_sim = numerator / demoninator
-    """
 
-    def loss(self, O, Y):
+    def set_optimizer(self, optimizer):
+        if optimizer  == 'adam':
+            self._update = adam
+        elif optimizer == 'adagrad':
+            self._update = adagrad 
+        elif optimizer == 'momentum':
+            self._update = momentum
+        elif optimizer == 'sgd':
+            self._update = sgd
+        elif optimizer == 'rms_clipped':
+            self._update = rmsprop_clipped
+        elif optimizer == 'sgd_clipped':
+            self._update = sgd_clipped
+        elif optimizer == 'rms':
+            self._update = rmsprop
+        else:
+            raise NotImplementedError("dont know what update this is...")
+
+    def save_word_vecs(self, save_path):
+        w = codecs.open(save_path, 'w', 'utf-8')
+        win, wout = self.get_params()
+        for voc_id, voc in self.vocab_model.id2voc.items():
+            w.write(voc + ' ' + ' '.join(map(str,win[voc_id,:].tolist())) + '\n')
+        w.flush()
+        w.close()
+
+    def load_model(self, load_path):
+        _params = [floatX(np.asarray(i)) for i in json.loads(open(load_path, 'r').read())]
+        W_in = theano.shared(floatX(_params[0]), name='W_in')
+        W_context = theano.shared(floatX(_params[1]), name='W_context')
+        return W_in, W_context
+
+    def save_model(self, save_path):
+        _params = json.dumps([i.get_value().tolist() for i in self.params])
+        f = open(save_path, 'w')
+        f.write(_params)
+        f.flush()
+        f.close()
+        return _params
+
+    def __xent_loss__(self, O, Y):
         return T.nnet.categorical_crossentropy(O, Y)
 
-    def nce_loss(self, O, Y, N):
+    def __nce_loss__(self, O, Y, N):
         # w is the next word in the training data
         pw = O[np.arange(0, self.batch_size), Y]
         qw = self.noise_dist[Y]
@@ -107,28 +121,65 @@ class BaseModel(object):
 
         return T.sum(T.log(pd1) + T.sum(T.log(pd0))) # scalar
 
-    @abstractmethod
-    def do_update(self, *params):
-        pass
-
-    @abstractmethod
-    def get_loss(self, *params):
-        pass
-
-    @abstractmethod
     def get_params(self,):
-        pass
+        return self.__params__()
 
-    @abstractmethod
-    def save_model(self,):
-        pass
+class SkipGram(BaseModel):
+    def __init__(self, vocab_model, batch_size, embed_size, saved_model = None, noise_sample_size = None, noise_dist = None, reg = 0.0, optimize = 'rms'):
+        BaseModel.__init__(self, 
+                vocab_model = vocab_model,
+                batch_size = batch_size, 
+                embed_size = embed_size, 
+                noise_sample_size = noise_sample_size, 
+                noise_dist = noise_dist,
+                reg = reg, 
+                optimize = optimize)
+        self.__computation_graph__()
 
-    @abstractmethod
-    def load_model(self, load_path):
-        pass
+    def __computation_graph__(self,):
+        lr = T.scalar('lr', dtype=theano.config.floatX) #for learning rates
+        X = T.lvector('X') #(batch_size)
+        Y = T.lvector('Y') #(batch_size)
+        #exception_verbosity=highN= T.lvector('N') #(noise_size)
 
-    def fit(self, epochs, batche_size, X, Y, N = None, noise_dist = None, noise_sample_size = None):
-        raise NotImplementedError("training routine should be inside base class?")
+        W_in = self.params[0] 
+        W_context = self.params[1] 
+
+        y_out_unnormalized = W_in[X].dot(W_context) #(batch_size, vocab_model.size)
+        y_pred = T.nnet.softmax(y_out_unnormalized)  #(batch_size, vocab_model.size)
+        model_losses = self.__xent_loss__(y_pred, Y) #T.nnet.categorical_crossentropy(y_pred, Y) #(batch_size,)
+        model_loss = model_losses.mean()
+        reg_loss = 0.
+        for rp in self.reg_params:
+            reg_loss += T.sum(T.sqr(rp))
+        loss = model_loss + (self.reg * reg_loss)
+
+        #optimization methods
+        self.__y_pred__ = theano.function(inputs = [X], outputs = y_pred)
+        self.__model_losses__ = theano.function(inputs = [X, Y], outputs = model_losses)
+        self.__loss__ = theano.function(inputs = [X, Y], outputs = [loss, model_loss, reg_loss])
+        self.__params__ = theano.function(inputs = [], outputs = self.params) 
+        self.__do_update__ = theano.function(inputs = [lr, X, Y], outputs = [loss, model_loss, reg_loss], updates = self._update(loss, self.params, lr)) 
+
+    def loss(self, batch_size, X, Y):
+        ave_dev_loss = []
+        t_idx = np.arange(X.shape[0])
+        batches = np.array_split(t_idx, X.shape[0] / batch_size)
+        for b_idx, batch_idxs in enumerate(batches):
+            _dev_batch_loss = self.__loss__(X[batch_idxs], Y[batch_idxs])
+            ave_dev_loss.append(_dev_batch_loss[0])
+        return np.mean(ave_dev_loss)
+
+    def fit(self, batch_size, learning_rate, X, Y):
+        t_idx = np.arange(X.shape[0])
+        np.random.shuffle(t_idx)
+        batches = np.array_split(t_idx, X.shape[0] / batch_size)
+        ave_loss = []
+        for b_idx, batch_idxs in enumerate(batches):
+            _batch_loss  = self.__do_update__(learning_rate, X[batch_idxs], Y[batch_idxs])
+            ave_loss.append(_batch_loss[0])
+        return np.mean(ave_loss)
+
 
 class CBOW(BaseModel):
     def __init__(self, vocab_model, batch_size, context_size, embed_size, saved_model = None, noise_sample_size = None, noise_dist = None, reg = 0.0, optimize ='rms'):
@@ -141,82 +192,53 @@ class CBOW(BaseModel):
                 reg = reg, 
                 optimize = optimize)
         self.context_size = context_size
-        self.make_network()
+        self.__computation_graph__()
 
-    def make_network(self,):
+    def __computation_graph__(self,):
         lr = T.scalar('lr', dtype=theano.config.floatX) #for learning rates
         X = T.lmatrix('X') #(batch_size, 2 * context_size)
         Y = T.lvector('Y') #(batch_size)
         #exception_verbosity=highN= T.lvector('N') #(noise_size)
 
-        #W_in = self.params[0] 
-        padded_W_in = T.concatenate((self.pad, self.params[0]))
-        #W_context = self.params[1] 
-        padded_W_context = T.concatenate((self.pad.T, self.params[1]), axis = 1)
+        W_in = self.params[0] 
+        W_context = self.params[1] 
 
-        w_in_x_sum = padded_W_in[X,:].sum(axis = 1) #T.sum(w_in_x, axis = 1) #(batch_size, hidden_state) # selects the rows in W_in, per row in X, then sums those rows
-        y_out_unnormalized = w_in_x_sum.dot(padded_W_context) #(batch_size, vocab_model.size)
+        w_in_x_sum = W_in[X,:].sum(axis = 1) #T.sum(w_in_x, axis = 1) #(batch_size, hidden_state) # selects the rows in W_in, per row in X, then sums those rows
+        y_out_unnormalized = w_in_x_sum.dot(W_context) #(batch_size, vocab_model.size)
         y_pred = T.nnet.softmax(y_out_unnormalized)  #(batch_size, vocab_model.size)
-        model_losses = T.nnet.categorical_crossentropy(y_pred, Y)
+        model_losses = self.__xent_loss__(y_pred, Y)
         model_loss = model_losses.mean()
-        #model_loss = self.nce_loss(y_pred, Y, N) #T.nnet.categorical_crossentropy(y_pred, Y).mean() #scalar
+        #model_losses = self.nce_loss(y_pred, Y, N) #T.nnet.categorical_crossentropy(y_pred, Y).mean() #scalar
         #model_loss = self.loss(y_pred, Y)
         reg_loss = 0.
         for rp in self.reg_params:
             reg_loss += T.sum(T.sqr(rp))
         loss = model_loss + (self.reg * reg_loss)
         #optimization methods
-        self.get_y_unnorm = theano.function(inputs = [X], outputs = y_out_unnormalized)
-        self.get_y_pred = theano.function(inputs = [X], outputs = y_pred)
-        self.get_model_losses_t = theano.function(inputs = [X, Y], outputs = model_losses)
-        self.get_loss_t = theano.function(inputs = [X, Y], outputs = [loss, model_loss, reg_loss])
-        self.get_params_t = theano.function(inputs = [], outputs = self.params) 
-        self.do_update_t = theano.function(inputs = [lr, X, Y], outputs = [loss, model_loss, reg_loss], updates = self._update(loss, self.params, lr)) 
+        self.__y_pred__ = theano.function(inputs = [X], outputs = y_pred)
+        self.__model_losses__ = theano.function(inputs = [X, Y], outputs = model_losses)
+        self.__loss__ = theano.function(inputs = [X, Y], outputs = [loss, model_loss, reg_loss])
+        self.__params__ = theano.function(inputs = [], outputs = self.params) 
+        self.__do_update__ = theano.function(inputs = [lr, X, Y], outputs = [loss, model_loss, reg_loss], updates = self._update(loss, self.params, lr)) 
 
-        #V1 = T.lscalar('V1')
-        #V2 = T.lscalar('V2')
-        #self.cosine_similarity = theano.function(inputs = [], outputs = self.cosine_sim) 
-        #self.cosine_similarity_cell = theano.function(inputs = [V1, V2], outputs = self.cosine_sim[V1, V2]) 
-        #self.cosine_similarity_row = theano.function(inputs = [V1], outputs = self.cosine_sim[V1,:]) 
+    def loss(self, batch_size, X, Y):
+        ave_dev_loss = []
+        t_idx = np.arange(X.shape[0])
+        batches = np.array_split(t_idx, X.shape[0] / batch_size)
+        for b_idx, batch_idxs in enumerate(batches):
+            _dev_batch_loss = self.__loss__(X[batch_idxs,:], Y[batch_idxs])
+            ave_dev_loss.append(_dev_batch_loss[0])
+        return np.mean(ave_dev_loss)
 
-    def get_loss(self, *params):
-        _X, _Y = params
-        return self.get_loss_t(_X, _Y),
-
-    def do_update(self, *params):
-        _lr, _X, _Y = params
-        return self.do_update_t(_lr, _X, _Y)
-
-    def get_params(self,):
-        return self.get_params_t()
-
-    def save_word_vecs(self, save_path):
-        w = codecs.open(save_path, 'w', 'utf-8')
-        win, wout = self.get_params()
-        for voc_id, voc in self.vocab_model.id2voc.items():
-            if voc_id == 0:
-                pass
-            else:
-                w.write(voc + ' ' + np.array2string(win[voc_id -1, :], separator = ' ', max_line_width = np.inf)[1:-1].strip() + '\n')
-        w.flush()
-        w.close()
-
-    def load_model(self, load_path):
-        sys.stderr.write('loading model' + load_path + '\n')
-        _params = [floatX(np.asarray(i)) for i in json.loads(open(load_path, 'r').read())]
-        W_in = theano.shared(floatX(_params[0]), name='W_in')
-        W_context = theano.shared(floatX(_params[1]), name='W_context')
-        return W_in, W_context
-
-    def save_model(self, save_path):
-        sys.stderr.write('saving model' + save_path + '\n')
-        _params = json.dumps([i.get_value().tolist() for i in self.params])
-        f = open(save_path, 'w')
-        f.write(_params)
-        f.flush()
-        f.close()
-        return _params
-
+    def fit(self, batch_size, learning_rate, X, Y):
+        t_idx = np.arange(X.shape[0])
+        np.random.shuffle(t_idx)
+        batches = np.array_split(t_idx, X.shape[0] / batch_size)
+        ave_loss = []
+        for b_idx, batch_idxs in enumerate(batches):
+            _batch_loss  = self.__do_update__(learning_rate, X[batch_idxs,:], Y[batch_idxs])
+            ave_loss.append(_batch_loss[0])
+        return np.mean(ave_loss)
 
 class Vocab(object):
     def __init__(self, vocab_file):
@@ -235,7 +257,7 @@ class Vocab(object):
             self.voc2count[voc] = int(voc_count)
             self.id2voc[int(voc_id)] = voc
         self.voc_dist = np.zeros(len(self.voc2id),)
-        self.size = len(self.voc2id) - 1 #-1 because of the PAD symbol 
+        self.size = len(self.voc2id) 
         for v,vid in self.voc2id.items():
             self.voc_dist[vid] = self.voc2count[v]
         self.voc_dist = self.voc_dist / self.voc_dist.sum()
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from models import CBOW, Vocab
-import pdb
+from models import CBOW, Vocab, SkipGram
 import codecs
 import numpy as np
 import argparse
@@ -33,8 +32,10 @@ if __name__ == '__main__':
     opt.add_argument('-e', action='store', dest='embed_size', type = int, required = True)
     opt.add_argument('--bs', action='store', dest='batch_size', default = 512, type = int)
     opt.add_argument('--epochs', action='store', dest='epochs', default = 10, type = int)
-    opt.add_argument('-m', action='store', dest='model', help='cbow or skipgram', required = True)
+    opt.add_argument('-m', action='store', dest='model', help='cbow or skipgram', choices = ['cbow', 'skipgram'], required = False, default = 'cbow')
+    opt.add_argument('-o', action='store', dest='optimizer', help='optimizer to use', choices = ['sgd', 'sgd_clipped', 'rms', 'rms_clipped'], required = False, default = 'rms')
     options = opt.parse_args()
+    print options
     vocab = Vocab(options.vocab_file)
     X_full, Y_full = load_data(options.training_data, options.model)
     if options.dev_data is None:
@@ -42,34 +43,29 @@ if __name__ == '__main__':
         np.random.shuffle(t_idx)
         d_idx = t_idx[:int(0.1 * t_idx.shape[0])]
         t_idx = t_idx[int(0.1 * t_idx.shape[0]):]
-        X_dev = X_full[d_idx,:]
-        Y_dev = Y_full[d_idx]
-        X_full = X_full[t_idx,:]
-        Y_full = Y_full[t_idx]
+        if options.model == 'cbow':
+            X_dev = X_full[d_idx,:]
+            Y_dev = Y_full[d_idx]
+            X_full = X_full[t_idx,:]
+            Y_full = Y_full[t_idx]
+        elif options.model == 'skipgram':
+            X_dev = X_full[d_idx]
+            Y_dev = Y_full[d_idx]
+            X_full = X_full[t_idx]
+            Y_full = Y_full[t_idx]
+        else:
+            raise BaseException("unknown model type")
     else:
         X_dev, Y_dev = load_data(options.dev_data, options.model)
-    print vocab.voc_dist.shape, X_full.shape, Y_full.shape
-    cbow = CBOW(vocab_model = vocab, batch_size = options.batch_size, context_size = X_full.shape[1], embed_size = options.embed_size, reg=0.0, optimize = 'rms')
-    epochs = options.epochs
-    for e_idx in xrange(epochs):
-        t_idx = np.arange(X_full.shape[0])
-        np.random.shuffle(t_idx)
-        batches = np.array_split(t_idx, X_full.shape[0] / options.batch_size)
-        ave_loss = []
-        for b_idx, batch_idxs in enumerate(batches):
-            _batch_loss  = cbow.do_update(0.01, X_full[batch_idxs,:], Y_full[batch_idxs])
-            #print e_idx, b_idx, _batch_loss[0]
-            ave_loss.append(_batch_loss[0])
-        print e_idx, 'train ave_loss:', np.mean(ave_loss)
-        ave_dev_loss = []
-        d_idx = np.arange(X_dev.shape[0])
-        batches = np.array_split(d_idx, X_dev.shape[0] / options.batch_size)
-        for b_idx, batch_idxs in enumerate(batches):
-            _dev_batch_loss = cbow.get_loss(X_dev[batch_idxs,:], Y_dev[batch_idxs])
-            ave_dev_loss.append(_dev_batch_loss[0][0])
-        print e_idx, 'dev ave_loss:', np.mean(ave_dev_loss)
+    if options.model == 'cbow':
+        model = CBOW(vocab_model = vocab, batch_size = options.batch_size, context_size = X_full.shape[1], embed_size = options.embed_size, reg=0.0, optimize = options.optimizer)
+    else:
+        model = SkipGram(vocab_model = vocab, batch_size = options.batch_size, embed_size = options.embed_size, reg=0.0, optimize = options.optimizer)
+    for e_idx in xrange(options.epochs):
+        print e_idx, 'training loss  :', model.fit(batch_size = options.batch_size, learning_rate = 0.01, X = X_full, Y = Y_full)
+        print e_idx, 'validation loss:', model.loss(batch_size = options.batch_size, X = X_dev, Y = Y_dev)
         if options.save_path is not None:
-            cbow.save_word_vecs(options.save_path + '.' + str(e_idx) + '.vecs')
-            cbow.save_model(options.save_path + '.' + str(e_idx) + '.model')
+            model.save_word_vecs(options.save_path + '.' + str(e_idx) + '.vecs')
+            model.save_model(options.save_path + '.' + str(e_idx) + '.model')
         else:
             pass
